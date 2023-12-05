6e052a338e8ce8acda06852a44bc58fbbaa63ea9
bean808 <100491569+bean808@users.noreply.github.com>
2023-12-05 21:40:30 +0100

    Can upload 1 photo now

--- app/src/main/java/hu/ait/traveldiary/ui/screen/add/AddEntryScreen.kt
+++ app/src/main/java/hu/ait/traveldiary/ui/screen/add/AddEntryScreen.kt
@@ -1,11 +1,18 @@
 package hu.ait.traveldiary.ui.screen.add
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
 import android.net.Uri
+import android.os.Build
+import android.provider.MediaStore
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
@@ -14,11 +21,13 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.core.content.FileProvider
@@ -30,6 +39,8 @@ import com.google.accompanist.permissions.rememberPermissionState
 import com.google.accompanist.permissions.shouldShowRationale
 import hu.ait.traveldiary.R
 import java.io.File
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 
 @OptIn(ExperimentalMaterial3Api::class, ExperimentalPermissionsApi::class)
@@ -39,15 +50,45 @@ fun AddEntryScreen(
 ) {
     var postTitle by remember { mutableStateOf("") }
     var postBody by remember { mutableStateOf("") }
+    var startDate by remember {
+        mutableStateOf(LocalDate.now())
+    }
+    var endDate by remember {
+        mutableStateOf(LocalDate.now())
+    }
 
+    val formattedStartDate by remember {
+        derivedStateOf{
+            DateTimeFormatter
+                .ofPattern("MM dd yyyy")
+                .format(startDate)
+        }
+    }
+
+    val formattedEndDate by remember {
+        derivedStateOf{
+            DateTimeFormatter
+                .ofPattern("MM dd yyyy")
+                .format(endDate)
+        }
+    }
+
+    var imageUri by remember {
+        mutableStateOf<Uri?>(null)
+    }
     val context = LocalContext.current
+
+    val bitmap = remember {mutableStateOf<Bitmap?>(null)}
+
+    val launcher = rememberLauncherForActivityResult(contract = ActivityResultContracts.GetContent()) { uri: Uri? ->
+        imageUri = uri
+    }
+
 
     var hasImage by remember {
         mutableStateOf(false)
     }
-    var imageUri by remember {
-        mutableStateOf<Uri?>(null)
-    }
+
     val cameraLauncher = rememberLauncherForActivityResult(
         contract = ActivityResultContracts.TakePicture(),
         onResult = { success ->
@@ -60,10 +101,31 @@ fun AddEntryScreen(
 
 
 
-
     Column(
         modifier = Modifier.padding(20.dp)
     ) {
+        imageUri?.let {
+            val source = ImageDecoder.createSource(context.contentResolver, it)
+            bitmap.value = ImageDecoder.decodeBitmap(source)
+
+            bitmap.value?.let { btm ->
+                Image(
+                    bitmap = btm.asImageBitmap(),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(400.dp)
+                        .padding(20.dp)
+                )
+            }
+        }
+
+        Spacer(modifier = Modifier.height(12.dp))
+
+        Button(onClick = { launcher.launch("image/*")}) {
+            Text(text = "upload an image")
+        }
+
+
         OutlinedTextField(value = postTitle,
             modifier = Modifier.fillMaxWidth(),
             label = { Text(text = "Post title") },
@@ -103,7 +165,6 @@ fun AddEntryScreen(
                 }
             }
         }
-
 
         if (hasImage && imageUri != null) {
             AsyncImage(model = imageUri,
