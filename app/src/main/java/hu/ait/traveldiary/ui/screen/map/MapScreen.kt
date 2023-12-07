8db5f2081c54cc804d17c2b262425f63aac934bf
Jangmin Song <jsong3@bates.edu>
2023-12-07 19:50:24 +0100

    Changes in mapscreen

--- app/src/main/java/hu/ait/traveldiary/ui/screen/map/MapScreen.kt
+++ app/src/main/java/hu/ait/traveldiary/ui/screen/map/MapScreen.kt
@@ -2,12 +2,7 @@ package hu.ait.traveldiary.ui.screen.map
 
 import android.Manifest
 import android.location.Location
-import android.view.Menu
-import android.view.MenuInflater
-import android.view.MenuItem
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -15,10 +10,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Info
@@ -30,11 +21,9 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
@@ -45,21 +34,14 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.layout
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.isGranted
 import com.google.accompanist.permissions.rememberPermissionState
 import com.google.accompanist.permissions.shouldShowRationale
-import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
@@ -69,7 +51,6 @@ import com.google.maps.android.compose.MapUiSettings
 import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.rememberCameraPositionState
-import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalPermissionsApi::class, ExperimentalMaterial3Api::class)
 @Composable
@@ -156,11 +137,11 @@ fun MapScreen(
         bottomBar = {
             NavigationBar(
                 containerColor = MaterialTheme.colorScheme.primaryContainer,
-            ){
-                Row (
+            ) {
+                Row(
                     modifier = Modifier
                         .align(Alignment.CenterVertically),
-                    ){
+                ) {
                     IconButton(
                         onClick = {
                             // Handle menu icon click
@@ -208,77 +189,68 @@ fun MapScreen(
         }
 
     ) {
+
+
         Column(modifier = Modifier.padding(it)) {
-//            val fineLocationPermissionState = rememberPermissionState(
-//                Manifest.permission.ACCESS_FINE_LOCATION
-//            )
-//        if (fineLocationPermissionState.status.isGranted) {
-//            Column {
-//
-//                Button(onClick = {
-//                    mapViewModel.startLocationMonitoring()
-//                }) {
-//                    Text(text = "Start location monitoring")
-//                }
-//                Text(
-//                    text = "Location: ${getLocationText(mapViewModel.locationState.value)}"
-//                )
-//            }
-//
-//        } else {
-//            Column() {
-//                val permissionText = if (fineLocationPermissionState.status.shouldShowRationale) {
-//                    "Please consider giving permission"
-//                } else {
-//                    "Give permission for location"
-//                }
-//                Text(text = permissionText)
-//                Button(onClick = {
-//                    fineLocationPermissionState.launchPermissionRequest()
-//                }) {
-//                    Text(text = "Request permission")
-//                }
-//            }
-//        }
+            val fineLocationPermissionState = rememberPermissionState(
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+            if (fineLocationPermissionState.status.isGranted) {
+                Column {
+                    Button(onClick = {
+                        mapViewModel.startLocationMonitoring()
+                    }) {
+                        Text(text = "Start location monitoring")
+                    }
+                    Text(
+                        text = "Location: ${getLocationText(mapViewModel.locationState.value)}"
+                    )
+
+                //This part. I also made two functions at the bottom (line 267-273) and tried
+                //LatLng(getLat(mapViewModel.locationState.value), getLng(mapViewModel.locationState.value)
+                //instead of mapViewModel.locationState.value!!.latitude and mapViewModel.locationState.value!!.longitude, but
+                //it still doesn't work. 
+//                    Marker(
+//                        state = MarkerState(
+//                            position = LatLng(
+//                                mapViewModel.locationState.value!!.latitude,
+//                                mapViewModel.locationState.value!!.longitude
+//                            )
+//                        ),
+//                        title = "Your Location"
+//                    )
+                }
+            } else {
+                Column() {
+                    val permissionText =
+                        if (fineLocationPermissionState.status.shouldShowRationale) {
+                            "Please consider giving permission"
+                        } else {
+                            "Give permission for location"
+                        }
+                    Text(text = permissionText)
+                    Button(onClick = {
+                        fineLocationPermissionState.launchPermissionRequest()
+                    }) {
+                        Text(text = "Request permission")
+                    }
+                }
+            }
+
 
             GoogleMap(
                 modifier = Modifier.fillMaxSize(),
                 cameraPositionState = cameraState,
                 properties = mapProperties,
-                uiSettings = uiSettings,
-
-//                onMapClick = {
-//                    mapViewModel.addMarkerPosition(it)
-//
-//                    //centers map
-//                    /*val cameraPosition = CameraPosition.Builder()
-//                        .target(it)
-//                        .build()*/
-////                val random = Random(System.currentTimeMillis())
-//                    val cameraPosition = CameraPosition.Builder()
-//                        .target(it)
-////                    .zoom(1f + random.nextInt(5))
-////                    .tilt(30f + random.nextInt(15))
-////                    .bearing(-45f + random.nextInt(90))
-//                        .build()
-//
-//                    coroutineScope.launch {
-//                        cameraState.animate(
-//                            CameraUpdateFactory.newCameraPosition(cameraPosition),
-//                            3000
-//                        )
-//                    }
-//                }
+                uiSettings = uiSettings
             ) {
 
-
-//                for (position in mapViewModel.getMarkersList()) {
-//                    Marker(
-//                        state = MarkerState(position = position), //should be it.latlng of the city
-//                        title = cityName //should be it.cityName
-////                    icon = bitmapDescriptor(context, R.drawable.)
-//                    )
-//                }
+                for (position in mapViewModel.getMarkersList()) {
+                    Marker(
+                        state = MarkerState(position = position), //should be it.latlng of the city
+                        title = cityName, //should be it.cityName
+                    )
+                }
 
             }
         }
@@ -287,10 +259,16 @@ fun MapScreen(
 
 fun getLocationText(location: Location?): String {
     return """
-       Lat: ${location?.latitude}
+       Lat: ${location?.latitude} 
        Lng: ${location?.longitude}
-       Alt: ${location?.altitude}
-       Speed: ${location?.speed}
-       Accuracy: ${location?.accuracy}
     """.trimIndent()
 }
+
+//fun getLat(location: Location): Double {
+//    return location.latitude
+//}
+//
+//fun getLng(location: Location): Double {
+//    return location.longitude
+//}
+
